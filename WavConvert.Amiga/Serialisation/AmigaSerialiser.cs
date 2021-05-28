5d8bf72107fd978715c0b66bc8355b22beb53bff
matburton <matburton@gmail.com>
2021-05-28 17:04:49 +0100

    Experimenting with a block format file
    Trying to work out which block type to use when seems pretty tricky!
    
    I'll see what the overhead is like if I just choose a block for each 64 gaps in turn and don't do any merges.
    
    This stupid design with only blocks 3 and 1 gets `DynamiteDan.fast.wav.amiga` down to ~100kB (from ~400kB).
    
    Reading this on the Amiga quickly enough could be tricky?

--- WavConvert.Amiga/Serialisation/AmigaSerialiser.cs
+++ WavConvert.Amiga/Serialisation/AmigaSerialiser.cs
@@ -22,87 +22,110 @@ namespace TapeTools.WavConvert.Amiga.Serialisation
 
             Console.WriteLine($"Total colour cycles:  {totalCycles:n0}");
 
-            var cyclesToIndex = colourCycleGaps
-                .GroupBy(c => c)
-                .OrderByDescending(g => g.Count())
-                .Select((g, i) => new { Cycles = g.Key, Index = i })
-                .Take(15)
-                .ToDictionary(r => r.Cycles, r => r.Index);
-
-            int? GetIndex(uint c) => cyclesToIndex.TryGetValue(c, out var index)
-                                   ? index : (int?)null;
-
-            return ToHeaderBytes(cyclesToIndex)
-                  .Append(BitConverter.GetBytes(startLow).Single())
-                  .Concat(GetBytes(colourCycleGaps, GetIndex))
+            return GetFileHeader(startLow)
+                  .Concat(GetFileBody(colourCycleGaps))
                   .ToArray();
         }
 
         private static uint ToColourCycles(TimeSpan pulseGap) =>
             Convert.ToUInt32(Math.Round(pulseGap.Ticks * 0.0709379));
 
-        private static IEnumerable<byte> GetBytes
-            (IReadOnlyList<uint> cycles, Func<uint, int?> getIndex)
+        private static IEnumerable<byte> GetFileHeader(bool startLow)
         {
-            var bytes = new List<byte>();
-
-            for (var index = 0; index < cycles.Count; index += 2)
-            {
-                var lastGap = index + 1 == cycles.Count;
-                
-                var currentIndex = getIndex(cycles[index]);
-
-                var nextIndex = lastGap ? null
-                              : getIndex(cycles[index + 1]);
-
-                bytes.Add(Convert.ToByte( ((currentIndex ?? 0xF) << 4)
-                                         | (nextIndex    ?? 0xF)));
-
-                if (currentIndex == null)
-                {
-                    bytes.AddRange(Get3Bytes(cycles[index]));
-                }
-
-                if (nextIndex == null && !lastGap)
-                {
-                    bytes.AddRange(Get3Bytes(cycles[index + 1]));
-                }
-            }
-
-            return bytes;
+            yield return BitConverter.GetBytes(startLow).Single();
         }
 
-        private static IEnumerable<byte> ToHeaderBytes
-            (IReadOnlyDictionary<uint, int> cyclesToIndex)
+        // Algorithm: Start with block type 3
+        // - How many gaps can a single block X reach? How many bytes is that?
+        // - How many bytes for block (X-1) followed by block X to reach that far?
+        // - If 2nd is smaller reject type X and retry with type (X-1)
+
+        // ^ Not ideal, could use a windowing algorithm?
+        //   i.e. only consider next, say, 64 gaps? Decide on block for that?
+        //   Then merge compatible adjacent blocks?
+        //   (If we didn't merge then, with 64, the block identifier could be one byte)
+        // ^ If so then should wrap each block behind an interface
+
+        // Types: 3 = Override block, can express any gaps as a stream of byte triples
+        //        2 = Quad block, can express stream of 4 unique gaps as stream of bit pairs
+        //        1 = Toggle block, can express stream of 2 unique gaps as stream of bits
+        //        0 = Repeat block, can express repetition of same gap as a count, no stream
+
+        private static IEnumerable<byte> GetFileBody(IReadOnlyList<uint> gaps)
         {
-            var bytes = new List<byte>();
+            const int maxBlockLength = 16_384; // 2 ^ 14 (14 bits)
 
-            var orderedCycles = cyclesToIndex.OrderBy(p => p.Value)
-                                             .Select(p => p.Key);
-
-            foreach(var cycles in orderedCycles)
+            while (gaps.Any())
             {
-                bytes.AddRange(GetBytes(cycles));
-            }
+                IEnumerable<byte> blockBytes;
 
-            for (var index = 15; index > cyclesToIndex.Count; --index)
-            {
-                bytes.AddRange(GetBytes(0)); // Padding
-            }
+                if (gaps.Take(3).Distinct().Count() <= 2)
+                {
+                    var seen = new HashSet<uint>();
 
-            return bytes;
+                    int UniqueSeen(uint gap) { seen.Add(gap);
+                                               return seen.Count; }
+                    var blockGaps = gaps
+                                   .TakeWhile(g => UniqueSeen(g) < 3)
+                                   .Take(maxBlockLength)
+                                   .ToArray();
+
+                    gaps = gaps.Skip(blockGaps.Length).ToArray();
+
+                    blockBytes = GetBlockIdentifier(1, blockGaps.Length)
+                                .Concat(GetToggleBlockBytes(blockGaps));
+                }
+                else
+                {
+                    var blockGaps = gaps
+                                   .Select((gap, index) => (gap, index))
+                                   .TakeWhile(t => gaps.Skip(t.index - 1).Take(3).Distinct().Count() is 3)
+                                   .Select(t => t.gap)
+                                   .Take(maxBlockLength)
+                                   .ToArray();
+
+                    gaps = gaps.Skip(blockGaps.Length).ToArray();
+
+                    blockBytes = GetBlockIdentifier(3, blockGaps.Length)
+                                .Concat(GetOverrideBlockBytes(blockGaps));
+                }
+
+                foreach (var blockByte in blockBytes)
+                {
+                    yield return blockByte;
+                }
+            }
+        }
+
+        private static IEnumerable<byte> GetBlockIdentifier(int type, int length) =>
+            BitConverter.GetBytes((short)((length << 2) | type)).Reverse();
+
+        private static IEnumerable<byte> GetOverrideBlockBytes
+            (IEnumerable<uint> gaps) => gaps.SelectMany(Get3Bytes);
+
+        private static IEnumerable<byte> GetToggleBlockBytes
+            (IReadOnlyCollection<uint> gaps)
+        {
+            var distinctGaps = gaps.Distinct().ToArray();
+
+            bool ToBool(uint gap) => gap != distinctGaps[0];
+
+            byte ToByte(IEnumerable<uint> gapGroup) => (byte)
+                gapGroup.Select(g => ToBool(g) ? 1 : 0)
+                        .Reverse()
+                        .Aggregate((a, b) => a << 1 | b);
+
+            var streamBytes = gaps.Select((gap, index) => (gap, index))
+                                  .GroupBy(t => t.index / 8, t => t.gap)
+                                  .Select(ToByte);
+
+            return distinctGaps.SelectMany(Get3Bytes).Concat(streamBytes);
         }
 
         private static IEnumerable<byte> Get3Bytes(uint cycles) =>
             GetBytes(cycles).Skip(1);
 
-        private static IEnumerable<byte> GetBytes(uint cycles)
-        {
-            var bytes = new byte[4];
-
-            BitConverter.TryWriteBytes(bytes, cycles);
-
-            return bytes.Reverse();
-        }
+        private static IEnumerable<byte> GetBytes(uint cycles) =>
+            BitConverter.GetBytes(cycles).Reverse();
     }
 }
